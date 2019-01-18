e0d868bb8059a42c7e1eaf19d487dd4ff6edef1f
andy <andy.lv.vip@hotmail.com>
2019-01-18 22:05:17 +0800

    add user register and adjust http security logic...

--- authserver/src/main/java/uca/security/auth/security/WebSecurityConfigurer.java
+++ authserver/src/main/java/uca/security/auth/security/WebSecurityConfigurer.java
@@ -3,7 +3,6 @@ package uca.security.auth.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,15 +40,4 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
         };
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .antMatcher("/**")
-                .authorizeRequests()
-                .antMatchers("/user/registry**")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-        ;
-    }
 }
