f29bc78f89744c467d0bb437ad9cd0470da78f73
andy.lv <lvhs@ucacc.com>
2019-01-19 13:30:42 +0800

    add spring rest docs...

--- authserver/src/test/java/test/auth/user/UserControllerTest.java
+++ authserver/src/test/java/test/auth/user/UserControllerTest.java
@@ -6,14 +6,19 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 import uca.security.auth.AuthApplication;
+import uca.security.auth.UserController;
 import uca.security.auth.domain.User;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
@@ -25,34 +30,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(classes = AuthApplication.class)
 @RunWith(SpringRunner.class)
+@Transactional
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
 public class UserControllerTest {
 
-    @Rule
-    public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
-
-    private MockMvc mockMvc;
-
     @Autowired
-    private WebApplicationContext context;
+    private MockMvc mockMvc;
 
     @Autowired
     private ObjectMapper mapper;
 
-    @Before
-    public void setUp() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-                .apply(documentationConfiguration(this.restDocumentation))
-                .build();
-    }
-
     @Test
-    public void register() throws Exception {
+    public void userRegister() throws Exception {
         User dummy = new User();
         dummy.setUsername("dummy");
         dummy.setPassword("password");
         this.mockMvc.perform(post("/user/register").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(dummy)))
                 .andExpect(status().isNoContent())
-        .andDo(document("register-user", requestFields(
+        .andDo(document("{method-name}", requestFields(
                 fieldWithPath("username").description("Login user name"),
                 fieldWithPath("password").description("Login password"),
                 fieldWithPath("createdOn").ignored()
